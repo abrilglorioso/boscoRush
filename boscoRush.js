270849d3ad96551431beece45f513fdbcd3f6a43
abrilglorioso <aglorioso@pioix.edu.ar>
2023-10-09 08:43:02 -0300

    casi terminamos

--- boscoRush.js
+++ boscoRush.js
@@ -1,8 +1,29 @@
 const bosco = document.getElementById("bosco");
+const pelota = document.getElementById("pelota");
 
 function jump() {
-    bosco.classList.add("jump");  
+    if (bosco.classList != "jump") {
+        bosco.classList.add("jump");
+
+        setTimeout(function () {
+            bosco.classList.remove("jump");
+        }, 300);
+    }
 }
+
+let isAlive = setInterval(function () {
+    let boscoTop = parseInt(window.getComputedStyle(bosco).getPropertyValue("top"));
+  
+    let pelotaLeft = parseInt(
+      window.getComputedStyle(pelota).getPropertyValue("left")
+    );
+
+    //detecta el choque
+    if (pelotaLeft < 45 && pelotaLeft > 0 && boscoTop >= 55) {
+        // choque
+        alert("Game Over!");
+      }
+}, 10);
 
 document.addEventListener("keydown", function (event) {
     jump();
