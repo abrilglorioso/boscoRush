4946453440b787b0040d795396b6b2975fd1d5c3
abrilglorioso <aglorioso@pioix.edu.ar>
2023-10-17 19:32:13 -0300

    ES LA QUE VAAAAA

--- boscoRush.js
+++
@@ -1,9 +0,0 @@
-const bosco = document.getElementById("bosco");
-
-function jump() {
-    bosco.classList.add("jump");  
-}
-
-document.addEventListener("keydown", function (event) {
-    jump();
-  });
